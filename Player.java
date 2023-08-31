3fc30d3c457f53bdf70a74b3f4a2fb09acff3f04
bakr2023-2024 <140170067+bakr2023-2024@users.noreply.github.com>
2023-08-31 13:47:06 +0300

    Add files via upload
    added raycaster code

---
+++ Player.java
@@ -0,0 +1,25 @@
+
+
+public class Player {
+	int FOV, hFOV;
+	double x, y;
+	int viewAng;
+	double playerSpeed;
+	double rotationSpeed;
+	boolean fishEyeOn;
+
+	public Player(int fOV, int viewAng, double playerSpeed, double rotationSpeed, boolean fishEyeOn) {
+		super();
+		FOV = fOV;
+		hFOV = FOV / 2;
+		this.viewAng = viewAng;
+		this.playerSpeed = playerSpeed;
+		this.rotationSpeed = rotationSpeed;
+		this.fishEyeOn = fishEyeOn;
+	}
+
+	void setSpawn(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
